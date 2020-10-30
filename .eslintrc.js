5388a641fa8f3881d020f607146288920af482c4
Milan Lazic <kockastajabuka@gmail.com>
2020-10-30 13:32:03 +0100

    First commit, installed some npm packages and configured Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'prettier'],
+  rules: {},
+}
